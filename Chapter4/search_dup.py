f9d8cb4ccdd54e67e43ad9d946ad19a5d3e9259d
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-01 21:11:01 -0400

    Added linear search program for checking duplicates.

---
+++ Chapter4/search_dup.py
@@ -0,0 +1,14 @@
+def search_dup(seq:list) -> bool:
+    existing_list = [0 for _ in range(max(seq)+1)]
+    for i in seq:
+        if existing_list[i] == 1:
+            print("Has duplicate")
+            return False
+        else:
+            existing_list[i] = 1
+    print("No duplicates")
+    return True
+        
+
+if __name__ == '__main__':
+    search_dup([1, 2, 3, 4, 5, 132])
