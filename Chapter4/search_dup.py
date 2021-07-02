002815018871af1ce60424d4311de30d465adeb5
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-01 21:22:39 -0400

    Added chap 4, exercise solution

--- Chapter4/search_dup.py
+++ Chapter4/search_dup.py
@@ -9,6 +9,15 @@ def search_dup(seq:list) -> bool:
     print("No duplicates")
     return False
         
+# Chapter 4, exercise question 4, O(n) solution.
+def greatest_number(seq: list) -> bool:
+    greatest_value = 0
+    for _ in seq:
+        if _ > greatest_value:
+            greatest_value = _
+    
+    print(f"Greatest Value: {greatest_value}")
 
 if __name__ == '__main__':
     search_dup([1, 2, 3, 4, 5, 132])
+    greatest_number([1, 2, 3, 4, 774, 132])
