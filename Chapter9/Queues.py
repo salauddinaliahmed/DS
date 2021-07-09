739a5a24c1d0bb7e6b2265fd25a3697f66992a7c
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-09 19:36:17 -0400

    Added Queue

---
+++ Chapter9/Queues.py
@@ -0,0 +1,33 @@
+class Queue:
+    def __init__(self, item=None):
+        self.q = []
+        if item:
+            self.enqueue(item)
+    
+    def enqueue(self, item):
+        self.q.append(item)
+    
+    def dequeue(self):
+        if self.q:
+            val = self.q[0]
+            self.q.remove(self.q[0])
+            return val
+        
+    def __repr__(self) -> str:
+        return f"Queue: {self.q}"
+
+    def read(self):
+        print(self.q[0])
+
+
+if __name__=='__main__':
+    queue = Queue()
+    for _ in range(5, 9):
+        queue.enqueue(_)
+    
+    print(queue)
+
+    for _ in range(5, 9):
+        queue.dequeue()
+
+    print(queue)
