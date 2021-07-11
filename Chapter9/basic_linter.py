fd851105077b6835c69143b2f3b8002452a694e4
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-10 22:43:59 -0400

    Added linter example

---
+++ Chapter9/basic_linter.py
@@ -0,0 +1,49 @@
+import Stacks
+
+class SimpleLinter:
+    def __init__(self, code):
+        self.code = code
+        self.s = Stacks.Stack()
+        self.braces = {
+            '}':"{",
+            ']':"[",
+            ')':'(',
+            '>':'<'
+        }
+        self.code_stack = self._create_stack()
+
+    def _create_stack(self):
+        all_open_braces = list(self.braces.keys())
+        all_open_braces.extend(self.braces.values())
+        all_braces = [x for x in self.code if x in all_open_braces]
+
+        for _ in all_braces:
+            if _ in list(self.braces.values()):
+                self.s.push(_)
+            
+            else:
+                last_brace_in_stack = self.s.pop()
+
+                if not last_brace_in_stack:
+                    print(f"Opening brace is missing for: {_}")
+                    return
+
+                if last_brace_in_stack != self.braces[_]:
+                    print(f"The brace {_} does not match with the closing brace: {self.braces[_]}") 
+                    return
+
+
+        # Stack is not empty, meaning we missed a closing bracket.
+        if x:=self.s.pop():
+            print(f"{x} does not have a closing backet.")
+            return
+
+        print("No errors in code!")
+
+if __name__ == "__main__":
+    code = 'function lala(){var x=5; console.log("Hello World");}'
+    SimpleLinter(code)
+
+
+
+        
