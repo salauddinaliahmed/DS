c3f93076e005c0ccd3320e896703cf394c778a0d
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-16 22:34:23 -0400

    Added factorial recursive function

--- Chapter10/recursion_eg.py
+++ Chapter10/recursion_eg.py
@@ -4,5 +4,14 @@ def rec_ex(num:int)->None:
         rec_ex(num-1)
     return
 
+
+def fact(num:int)->None:
+    if num <= 1:
+        return 1
+    return num * fact(num-1)
+
+
 if __name__=='__main__':
-    rec_ex(10)
+    #rec_ex(10)
+    x = fact(5)
+    print(x)
