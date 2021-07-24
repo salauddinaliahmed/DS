9c989d6c02eb09114ee5fadd741e1b7b56cfd94a
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-24 00:17:04 -0400

    Added better recursion as well

--- Chapter12/more_recursion.py
+++ Chapter12/more_recursion.py
@@ -12,7 +12,20 @@ def bad_recursion_max(l):
     else:
         return bad_recursion_max(l[1:])
 
+def better_recursion_max(l):
+    if len(l) == 1:
+        return l[0]
+
+    max_item = better_recursion_max(l[1:])
+    
+    if l[0] > max_item:
+        return l[0]
+    
+    else:
+        return max_item
 
 
 if __name__ == '__main__':
     print(bad_recursion_max([1, 2, 3, 4]))
+    print("_"*10)
+    print(better_recursion_max([1,5,3,4]))
