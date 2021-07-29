a7d8c34a10ef66908389305053c35be6c68216aa
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-07-28 23:01:20 -0400

    Added one line this time around, but read through the QuickSort Algorithm:

---
+++ Chapter13/quick_sort.py
@@ -0,0 +1,9 @@
+"""
+Quick Sort:
+- Uses a concept called partitioning, which is to take a random value in the array (called the pivot), then we make sure 
+  that every number that is less than the pivot ends at the left side of the pivot and 
+  every number at that is greater than the pivot is on the right side.
+
+This uses a combination of recursion and looping.
+
+"""
