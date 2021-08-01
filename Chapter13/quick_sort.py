7cb9d9804b2985d7bd6d2b6d0ba0222bca84570b
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-01 17:12:49 -0400

    Redoing quick sort, for better understanding

--- Chapter13/quick_sort.py
+++ Chapter13/quick_sort.py
@@ -1,72 +1,36 @@
-"""
-Quick Sort:
-- Uses a concept called partitioning, which is to take a random value in the array (called the pivot), then we make sure 
-  that every number that is less than the pivot ends at the left side of the pivot and 
-  every number at that is greater than the pivot is on the right side.
+class QuickSort:
+  def __init__(self, array) -> None:
+    self.array = array
+    self.count = 0
 
-This uses a combination of recursion and looping.
+  def partition(self, left, right) -> int:
+    self.count += 1
+    return 6
 
-"""
-from math import pi
-import random
-arr = [0, 5, 2, 1, 6, 3]
+  def _do_quick_sort(self, left, right):
+    self.count += 1
+    # Wrong condition
+    if right - left >= 0:
+      return
+    
+    pivot = self.partition(left, right)
 
+    # Parition and sort left side of the pivot
+    self._do_quick_sort(left, pivot-1)
 
-# def quicksort(arr):
-#   # Randomly select pivot (Pivot is an index).
-#   arr_size = len(arr)-1
-#   # pivot = random.randint(0, arr_size)
-#   pivot = 5
-#   print(f"The pivot is: {pivot} and it holds the value: {arr[pivot]}")
-
-#   left_pointer = 0
-#   right_pointer = arr_size-1
-
-#   result = pivot_compare(arr, pivot, left_pointer, right_pointer)
-#   return result
-
-
-def pivot_compare(left_pointer, right_pointer):
-  global arr
-
-  pivot = right_pointer
-  right_pointer -= 1
-
-  while True:
-    while arr[left_pointer] < arr[pivot]:
-    # Move to right unless you encounter a value greater than or equal to the pivot.
-      left_pointer += 1
-
-    # Everything to the right of the pivot, should be greater than the pivot
-    while arr[right_pointer] > arr[pivot]:
-      right_pointer -= 1
-
-
-    # Check if left pointer has gone beyond right pointer.
-    if left_pointer >= right_pointer:
-      # Swap the pivot
-      arr[pivot], arr[left_pointer] = arr[left_pointer], arr[pivot]
-      print(f"This is the left pointer: {left_pointer}")
-      break
-
-    else:
-      arr[left_pointer], arr[right_pointer] = arr[right_pointer], arr[left_pointer]
-
-  return left_pointer
-
-def quick_sort(left, right):
-  if right - left <= 0:
-    return
+    # Parition and sort right side of the pivot
+    self._do_quick_sort(pivot+1, right)
   
-  pivot = pivot_compare(left, right)
 
-  quick_sort(left, pivot-1)
-  quick_sort(pivot+1, right)
-
-  
+  def quick_sort(self):
+    self._do_quick_sort(0, len(self.array)-1)
+    return self.array
 
 
 if __name__ == '__main__':
-  print(f"Old array: {arr}")
-  quick_sort(0, len(arr)-1)
-  print(f"Sorted array: {arr}")
+  arr = [0,5,2,1,6,3]
+  print(f"Array before sorting: {arr}")
+  qs = QuickSort(arr)
+  print(f"Array after quick sort: {qs.quick_sort()}")
+  print("\n")
+  print(f"Total steps taken for an array of size {len(arr)} is {qs.count}")
