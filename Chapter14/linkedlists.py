2c6e765460e365d24a607f59d11deaa3c458bf9f
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-05 21:31:35 -0400

    having trouble with delete

--- Chapter14/linkedlists.py
+++ Chapter14/linkedlists.py
@@ -13,9 +13,9 @@ class LinkedList:
     def __str__(self) -> str:
         current_node = self.first_node
         output = ''
-        while current_node.next_node:
-            current_node = current_node.next_node
+        while current_node:
             output += str(current_node.data) + ', '
+            current_node = current_node.next_node
         return output
 
     def _goto_node(self, ind):
@@ -33,22 +33,26 @@ class LinkedList:
 
     # insert method
     def insert(self, val, ind=0):
-        current_node = self.first_node
-        current_index = 0
-        if not ind:
-            while current_node.next_node:
-                current_node = current_node.next_node
-            current_node.next_node = Node(val)
+        # If the first node is empty and there is no next node. 
+        if self.first_node.data == None and self.first_node.next_node == None:
+            self.first_node.data = val
+            return
         
-        else:
-            current_node = self._goto_node(ind-1)
-
-            next_node = current_node.next_node
+        # Check if index was provided.
+        if ind:
+            previous_node = self._goto_node(ind-1)
             new_node = Node(val)
-
-            current_node.next_node = new_node
-            new_node.next_node = next_node
+            new_next_node = previous_node.next_node
+            new_node.next_node = new_next_node
+            previous_node.next_node = new_node
+            return
         
+        current_node = self.first_node
+        while current_node:
+            previous_node = current_node
+            current_node = current_node.next_node
+        previous_node.next_node = Node(val)
+
 
     # Read at index:
     def read(self, ind):
@@ -56,11 +60,22 @@ class LinkedList:
 
     # Search
     def search(self, val):
-        pass
+        index = 0
+        if val == self.first_node.data:
+            return index
+        current_node = self.first_node.next_node
+
+        while current_node:
+            index += 1
+            if current_node.data == val:                
+                return index
+            current_node = current_node.next_node
+        print("Element not found.")
 
     def delete(self, ind):
         prev_node = self._goto_node(ind-1)
         delete_node = prev_node.next_node
+        print(f"Node to delete: {delete_node.data}")
         next_node = prev_node.next_node.next_node
 
         prev_node.next_node = next_node
@@ -78,3 +93,5 @@ if __name__ == '__main__':
     ll.delete(1)
     print("After delete")
     print(ll)
+    print("Search...")
+    print(ll.search(4))
