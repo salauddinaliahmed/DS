02d14566ac4ca05e2da2eceec7f52ba2028cdef4
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-07 23:23:37 -0400

    Added insert method for double linked list.

--- Chapter14/doublelinkedlist.py
+++ Chapter14/doublelinkedlist.py
@@ -7,5 +7,32 @@ class Node:
 
 
 class DoubleLinkedList:
-    def __init__(self, data) -> None:        
+    def __init__(self, data=None) -> None:        
         self.first_node = Node(data)
+    
+    def __str__(self):
+        current_node = self.first_node
+        output = f"{self.first_node.data}, " if self.first_node.data else ''
+        while current_node.right:
+            current_node = current_node.right
+            output += f'{str(current_node.data)}, '
+        return output
+
+    def insert(self, data):
+        current_node = self.first_node
+        new_node = Node(data)
+        while current_node.right:
+            current_node = current_node.right
+        
+        new_node.left = current_node
+        current_node.right = new_node
+
+
+if __name__ == '__main__':
+    dd = DoubleLinkedList()
+    print(dd)
+    dd.insert(10)
+    dd.insert(40)
+    dd.insert(50)
+    print(dd)
+    
