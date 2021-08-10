8102b7d26c67c202f0cd5d4d895d542f4e5e7e74
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-09 23:50:03 -0400

    Added double linked list methods.

--- Chapter14/doublelinkedlist.py
+++ Chapter14/doublelinkedlist.py
@@ -10,6 +10,10 @@ class DoubleLinkedList:
     def __init__(self, data=None) -> None:        
         self.first_node = Node(data)
         self.last_node = Node(data)
+
+        # This logic links first and the last node.
+        self.first_node.right = self.last_node
+        self.last_node.left = self.first_node
     
     def __str__(self):
         current_node = self.first_node
@@ -20,24 +24,44 @@ class DoubleLinkedList:
         return output
 
     def print_reverse(self):
-        current_node = self.first_node
-        pass
+        current_node = self.last_node
+        output = ''
+        while current_node.left:
+            output += str(current_node.data) + ', '
+            current_node = current_node.left
+        output += str(self.first_node.data)
+        print (output)
 
     def insert(self, data):
         current_node = self.first_node
-        new_node = Node(data)
-        while current_node.right:
-            current_node = current_node.right
-        
-        new_node.left = current_node
-        current_node.right = new_node
+        if self.first_node.data == None:
+            self.first_node.data = data
+        else:
+            while current_node.right:
+                current_node = current_node.right
 
+            if current_node.data == None:
+                current_node.data = data
+            else:
+                new_node = Node(data)        
+                new_node.left = current_node
+                current_node.right = new_node
+                self.last_node = new_node
+
+    def insert_at_end(self, data):
+        current_node = self.last_node
+        new_node = Node(data)
+        self.last_node.right = new_node
+        new_node.left = self.last_node
+
+        self.last_node = new_node
 
 if __name__ == '__main__':
     dd = DoubleLinkedList()
-    print(dd)
     dd.insert(10)
     dd.insert(40)
     dd.insert(50)
     print(dd)
-    
+    dd.print_reverse()
+    dd.insert_at_end(58)
+    print(dd)
