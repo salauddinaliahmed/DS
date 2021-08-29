ca6fbd74d0b692d299e51aeedc79759526bf05a0
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-28 23:44:19 -0400

    Working heap with array under the hood; or should i say lists.

--- Chapter16/heaps.py
+++ Chapter16/heaps.py
@@ -42,15 +42,71 @@ class Heap:
 
             self.heap_arr[self.parent_of(ind_last)], self.heap_arr[ind_last] = self.heap_arr[ind_last], self.heap_arr[self.parent_of(ind_last)]
             ind_last = self.parent_of(ind_last)
-            
+
+
+    def greatest_sibling(self, ind):
+        left = (ind*2) + 1
+        right = (ind*2) + 2
+        max_len = len(self.heap_arr)
+
+        if right > max_len and left <= max_len:
+            return left
+
+        else:
+            if self.heap_arr[right] > self.heap_arr[left]:
+                return right
+            else:
+                return left
+
+    def has_greater_child(self, ind):
+        left = self.left_child_of(ind)
+        right = self.right_child_of(ind)
+        
+        # Because we are indexing, we need to check if we have an indexable value.
+        max_len = len(self.heap_arr) - 1
+
+        if right > max_len:
+            if left > max_len:
+                return False
+            else:
+                return self.heap_arr[left] > self.heap_arr[ind]
+
+        else:
+            return self.heap_arr[left] > self.heap_arr[ind] or self.heap_arr[right] > self.heap_arr[ind]
+
 
     def delete(self):
         """Only deletes the root node."""
-        pass
+        root_node = self.first_node
+        self.heap_arr[0] = self.last_node
+
+        # Pop defaults to last value. Delete the last value.
+        self.heap_arr.pop()
+
+        print(f"Heap after delete: {self.heap_arr}")
+        current_node = 0
+
+        while current_node < len(self.heap_arr) and self.has_greater_child(current_node):
+            
+            gc = self.greatest_sibling(current_node)
+            self.heap_arr[current_node], self.heap_arr[gc] = self.heap_arr[gc], self.heap_arr[current_node]
+            current_node = gc
+    
+        return root_node
+
+
+
+
+
+
+        
 
 
 if __name__ == '__main__':
     h = Heap(10)
     h.insert(4)
+    h.insert(7)
     h.insert(15)
+    print(h)
+    print(h.delete())
     print(h)
