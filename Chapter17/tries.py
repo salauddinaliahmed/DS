791b27fefcec22718aca8abbee1a8e62dd2acdcc
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-30 23:17:53 -0400

    read about trie search

--- Chapter17/tries.py
+++ Chapter17/tries.py
@@ -11,4 +11,13 @@ class Trie:
         """Creates an empty TrieNode and saves it as root."""
         self.root = TrieNode()
     
+    def search(self, word):
+        """We iterate through the word and check for each character in the trie
+            each character is a key with the value as another trie node with the next set of characters.
+            level 1 c
+            level 2 a 
+            level 3 t 
+        """
+        pass
+    
 
