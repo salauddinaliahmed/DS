37813e3debfa45df9e0221b3558f3511d923f33b
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-08-31 23:25:59 -0400

    Added insert logic

--- Chapter17/tries.py
+++ Chapter17/tries.py
@@ -11,6 +11,9 @@ class Trie:
         """Creates an empty TrieNode and saves it as root."""
         self.root = TrieNode()
     
+    def __str__(self):
+        return f'Roots children: {self.root.children.keys()}'
+
     def search(self, word):
         """We iterate through the word and check for each character in the trie
             each character is a key with the value as another trie node with the next set of characters.
@@ -20,4 +23,19 @@ class Trie:
         """
         pass
     
+    def insert(self, word):
+        current_node = self.root
+        for each_letter in word:
+            if n := current_node.children.get(each_letter, False):
+                current_node = n
+            else:
+                current_node.children[each_letter] = current_node =  TrieNode()
+        
+        current_node.children['*'] = None
 
+
+if __name__ == '__main__':
+    tr = Trie()
+    tr.insert('cat')
+    tr.insert('bat')
+    print(tr)
