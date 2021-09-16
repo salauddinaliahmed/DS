e92c1a1d955f0c5ab158b56007d66c7ce33f497c
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-09-15 22:05:59 -0400

    Starting implementation of dijkstras algorithm

--- Chapter18/Dijkstra_algo.py
+++ Chapter18/Dijkstra_algo.py
@@ -1,4 +1,22 @@
-class DikjAlgo:
-    """Trying to learn the dijkstra's algorithm
-        Additional code will be added in 2 days.
-    """
+from weighted_graphs import WeightedGraph
+
+def dijkstras_algo(source, destination):
+    pass
+
+
+if __name__ == '__main__':
+    atlanta = WeightedGraph('Atlanta')
+    boston = WeightedGraph('Boston')
+    chicago = WeightedGraph('Chicago')
+    denver = WeightedGraph('Denver')
+    el_paso = WeightedGraph('El Paso')
+
+    atlanta.add_adjecent_vertex(boston, 100)
+    atlanta.add_adjecent_vertex(denver, 160)
+    boston.add_adjecent_vertex(chicago, 120)
+    boston.add_adjecent_vertex(denver, 180)
+    chicago.add_adjecent_vertex(el_paso, 80)
+    denver.add_adjecent_vertex(chicago, 40)
+    denver.add_adjecent_vertex(el_paso, 140)
+
+    print(atlanta)
