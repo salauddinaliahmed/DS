2679758667081cdafda56144d2fa7152005eae43
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-09-25 22:24:10 -0400

    Read a few pages of the final chapter

--- chapter19.py
+++ chapter19.py
@@ -6,6 +6,13 @@ def rev_string(s):
         s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
 
     print (s)
+# Read about the framework to use for optimizing an algorithm.
+"""
+1. Check the current complexity / performance of the algo.
+2. Come up with the best logically possible complexity.
+3. Incase the best complexity is better than the current complexity. 
+    Try to get the algo either to the best estimate or something between the best estimate or the current estimate.
+"""
 
 
 if __name__ == '__main__':
