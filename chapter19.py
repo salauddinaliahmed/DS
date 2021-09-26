89a9f340fd7768ff6d8b4215a76e4c32aa5d3970
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-09-26 17:53:44 -0400

    Added greedy algo (simple)

--- chapter19.py
+++ chapter19.py
@@ -12,8 +12,21 @@ def rev_string(s):
 2. Come up with the best logically possible complexity.
 3. Incase the best complexity is better than the current complexity. 
     Try to get the algo either to the best estimate or something between the best estimate or the current estimate.
+
+4. Read about looking at the algo and pondering if the act of constant time retrieval (through a hash table) would speed up the algo.
+5. Read about greedy approach where the currently available option is selected as the best option.
+   for eg: When finding the greatest value in an array, you could sort it and then return greatest elem (nLogn) or you could make the first 
+   element as the greatest and iterate through the array once, and swap the greatest value if we encounter a value greater than the earlier value.
+
 """
+def greedy_search(s):
+    greatest_num = 0
+    for each in s:
+        if each > greatest_num:
+            greatest_num = each
+    return greatest_num
 
 
 if __name__ == '__main__':
     rev_string([1, 4, 5, 2])
+    print(greedy_search([1,2, 5, 3]))
