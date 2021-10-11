8dbfb2a17275383fc73c942d9692d6dbf70c6bf7
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2021-10-10 23:24:20 -0400

    Wrote a rough version of longest sequence

--- Chaper20.py
+++ Chaper20.py
@@ -35,11 +35,50 @@ def longest_sequence(arr):
         - If it is, then increase count by one (Loop through incrementing one each time until you dont find it)
         - Keep track of the count, if its great next time you update. 
     """
-    pass
-    
+    longest_sequence = 0
+    new_dict = {}
+    max_number = 0 
+    min_number = float('inf')
+
+    for _ in arr:
+        if _ > max_number:
+            max_number = _
+        elif _ < min_number:
+            min_number = _
+
+    for i in range(min_number, max_number+1):
+        new_dict[i] = 6
+
+    for _ in arr:
+        if new_dict.get(_):
+            new_dict[_] = 1
+
+    new_dict_sorted = []
+    for each_key, each_val in new_dict.items():
+        if each_val == 1:
+            new_dict_sorted.append(each_key)
+
+    start_val = new_dict_sorted[0]
+    max_l = len(new_dict_sorted)
+    i = 1
+    current_longest_seq = 0
+    while i < max_l:
+        if new_dict_sorted[i] == start_val + 1:
+            current_longest_seq += 1
+        else:
+            if current_longest_seq > longest_sequence:
+                longest_sequence = current_longest_seq
+            current_longest_seq = 0
+        
+        start_val = new_dict_sorted[i]
+        i += 1
+
+    print("Current_Longest_sequence: ", longest_sequence)
+    print(new_dict_sorted)
 
 
 
 if __name__ == '__main__':
-    print(temp_sort([98.6, 98.0, 99.0, 98.9, 95.5, 98.2, 98.0, 97.1]))
+    # print(temp_sort([98.6, 98.0, 99.0, 98.9, 95.5, 98.2, 98.0, 97.1]))
     # What i missed in the earlier implementation is that my algo does not respect duplicates.
+    longest_sequence([13, 54, 50, 12, 14, 115])
